ae4547b65c4635cc254f6eb4812ed295a8a87a19
lokada <lyokada@ucsc.edu>
2014-12-06 23:02:20 -0800

    finished user edit, delete, admin authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  # before_action :logged_in_user, only: [:edit, :update]
   protect_from_forgery with: :exception
   include SessionsHelper
 end
